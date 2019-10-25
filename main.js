d53b9cffc9197d6f80448245cb7430a87c72d5ca
JMoney-216 <jdm216@gmail.com>
2019-10-25 11:48:59 -0500

    board has elements but isn't layed out correctly, started JS, added event listener that works when clicking on board.

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hello")
+/*----- constants -----*/
+const gameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+/*----- app's state (variables) -----*/
+let player1;
+let player2;
+let playedGrid = [];
+
+/*----- cached element references -----*/
+let board = document.getElementById('board-container');
+
+/*----- event listeners -----*/
+board.addEventListener('click', function(event) {
+    event.preventDefault();
+    let clickedGrid = event.target;
+    if(clickedGrid.tagName !== 'P') return;
+    console.log(clickedGrid)
+}) 
+    
+
+
+/*----- functions -----*/
+init() {
+    player1 = `X's`;
+    player2 = `O's`;
+}
